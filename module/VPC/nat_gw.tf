61b9df3ffe31b7ad45cab93e529a3b97d4a8e633
a1edem <edemosdjoss@yahoo.fr>
2025-04-02 19:11:03 -0500

    togo-terra

--- module/VPC/nat_gw.tf
+++ module/VPC/nat_gw.tf
@@ -1,28 +1,27 @@
+# variable "env" {
+#   type    = string
+#   default = "prod"
+# }
 
-# # variable "env" {
-# #   type    = string
-# #   default = "prod"
-# # }
+# Create Elastic IPs for NAT Gateways
+resource "aws_eip" "nat" {
+  for_each = aws_subnet.public # One EIP per public subnet
+  tags = {
+    Name = format("eip-nat-%s", each.key)
+  }
+}
 
-# # # Create Elastic IPs for NAT Gateways
-# # resource "aws_eip" "nat" {
-# #   for_each = aws_subnet.public # One EIP per public subnet
-# #   tags = {
-# #     Name = format("eip-nat-%s", each.key)
-# #   }
-# # }
+# Create NAT Gateways
+resource "aws_nat_gateway" "nat" {
+  for_each = aws_subnet.public # One NAT Gateway per public subnet
 
-# # # Create NAT Gateways
-# # resource "aws_nat_gateway" "nat" {
-# #   for_each = aws_subnet.public # One NAT Gateway per public subnet
+  allocation_id = aws_eip.nat[each.key].id # Use the EIP for the NAT Gateway
+  subnet_id     = each.value.id            # Associate with the public subnet
 
-# #   allocation_id = aws_eip.nat[each.key].id # Use the EIP for the NAT Gateway
-# #   subnet_id     = each.value.id            # Associate with the public subnet
-
-# #   tags = {
-# #     Name = format("nat-gateway-%s", each.key)
-# #   }
-# # }
+  tags = {
+    Name = format("nat-gateway-%s", each.key)
+  }
+}
 
 # # Create Elastic IPs for NAT Gateways
 # resource "aws_eip" "nat" {
